bf753427fbdfe92469b5d573ab7a68d91b6c7522
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-17 16:22:14 +0300

    Rename

--- PrintTxt.java
+++ PrintTxt.java
@@ -1,6 +1,6 @@
 // Напишите программу, выводящую в консоль It's alive! It's alive!
 
-public class Print_txt {
+public class PrintTxt {
   public static void main(String[] args) {
     System.out.println("It's alive! It's alive!");
   }
