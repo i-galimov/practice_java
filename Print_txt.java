54e61d2a9e9daf4872e44c66aff8ababb55ea2a3
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-17 16:16:27 +0300

    Rename with big P

--- Print_txt.java
+++ Print_txt.java
@@ -1,6 +1,6 @@
 // Напишите программу, выводящую в консоль It's alive! It's alive!
 
-public class print_txt {
+public class Print_txt {
   public static void main(String[] args) {
     System.out.println("It's alive! It's alive!");
   }
