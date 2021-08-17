329216d9fbc4ed4b3f485d94b80eca076f4ddf7f
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-17 16:08:53 +0300

    Rename class

--- print_txt.java
+++ print_txt.java
@@ -1,6 +1,6 @@
-// Напишите программу, выводящую в консоль It's alive! It's alive! Класс должен называться "Main", это ограничение проверяющей системы.
+// Напишите программу, выводящую в консоль It's alive! It's alive!
 
-public class Main {
+public class print_txt {
   public static void main(String[] args) {
     System.out.println("It's alive! It's alive!");
   }
