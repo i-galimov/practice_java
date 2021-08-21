8055fc9d043904fec53b98e51186bb8c09d64363
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-08-21 14:16:07 +0300

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,19 @@
+/* Создайте четыре переменных разного типа и выведите их на печать, каждое на новой строке. Присвойте переменным значения:
+1)  2021
+2)  3.1415
+3)  Java
+4) 5  < 10
+Выведите значения переменных столбиком в этом же порядке. */
+
+class Variables {
+    public static void main(String[] args) {
+        int a = 2021;
+        double b = 3.1415;
+        String c = "Java";
+        boolean d = 5 < 10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+  }
+}
