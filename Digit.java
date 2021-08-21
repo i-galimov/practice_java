6ddddc9d2196e7ca84de6e51c2b3897fe5cfe478
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-08-21 13:59:20 +0300

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,15 @@
+// Напишите программу, которая присваивает трем переменным значения 100, 200 и 300. Выведите на печать значения переменных в столбик, а затем в строку через пробел.
+
+class Digit {
+public static void main(String[] args) {
+        int a = 100, b = 200, c = 300;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.print(a);
+        System.out.print(" ");
+        System.out.print(b);
+        System.out.print(" "); 
+        System.out.print(c);
+  }
+}
