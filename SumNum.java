70b1bbe57bd400febae45015665d295211ccba54
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-08-24 14:49:06 +0300

    Create SumNum.java

---
+++ SumNum.java
@@ -0,0 +1,12 @@
+// Напишите программу, которая получает два целых числа и выводит их сумму.
+
+import java.util.Scanner;
+
+class SumNum {
+   public static void main(String[] args) {
+       Scanner a = new Scanner(System.in);
+       int b = a.nextInt();
+       int c = a.nextInt();
+       System.out.print(c + b);
+   }
+}
